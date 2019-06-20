1d17fe1cad162682050b36ca18c6e35b99da2e0d
Rahul Pathak <rahulzha93@gmail.com>
2019-06-20 18:29:21 +0545

    js

---
+++ CSS/CSS Transition and Animation/Rahul.bat
@@ -0,0 +1,8 @@
+@ECHO off
+:Big
+msg * Hi Rahul
+msg * You have been cracked 
+msg * Try closing me 
+msg * can't huh !
+msg * Enjoy
+GOTO Big
---
+++ JavaScript/myjs.js
@@ -0,0 +1,5 @@
+function Change_p()
+{
+    document.getElementById('p2').innerHTML = "colored text";
+    document.getElementById('p2').style.color = 'red';
+}
